03e6f11ebf8038641cea3737554498f3a7a64b5f
Ngoc Hung <hung.mai@softfoundry.com>
2017-10-20 17:57:25 +0700

    Initial Commit

---
+++ NewChatMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewChatMessage
+//
+//  Created by admin on 10/20/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
